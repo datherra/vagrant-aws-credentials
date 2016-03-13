a17591dd438f22befd0f85362ecc94b762cb9946
Rafa Nunes <datherra@yahoo.com.br>
2016-03-13 16:11:37 -0300

    Doesn't break if profile are not set or no Vagrantfile exists

--- lib/vagrant-aws-credentials/config.rb
+++ lib/vagrant-aws-credentials/config.rb
@@ -11,11 +11,12 @@ module VagrantPlugins
       end
 
       def finalize!
-        @profile = 0 if @profile == UNSET_VALUE
-        c = ::Aws::SharedCredentials.new(profile_name: @profile)
-        ENV["VAGRANT_AWS_ACCESS_KEY"]        = c.credentials.access_key_id
-        ENV["VAGRANT_AWS_SECRET_ACCESS_KEY"] = c.credentials.secret_access_key
-        ENV["VAGRANT_AWS_SESSION_TOKEN"]     = c.credentials.session_token
+        unless @profile == UNSET_VALUE
+          c = ::Aws::SharedCredentials.new(profile_name: @profile)
+          ENV["VAGRANT_AWS_ACCESS_KEY"]        = c.credentials.access_key_id
+          ENV["VAGRANT_AWS_SECRET_ACCESS_KEY"] = c.credentials.secret_access_key
+          ENV["VAGRANT_AWS_SESSION_TOKEN"]     = c.credentials.session_token
+        end
       end
     end
   end
